abbc5ccc6adced553d920d681e3671367b23140d
vidyachandasekhar <vidyahari.vs@Gmail.com>
2017-06-17 22:09:48 -0400

    added header/ item/ footer / images using Angular4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { SearchItemComponent } from './search-item/search-item.component';
+import { ItemComponent } from './item/item.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SearchItemComponent,
+    ItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
